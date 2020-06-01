9bbeaa2a128c3047e156c1b5344f1623713a76fc
Justin Vrana <justin.vrana@gmail.com>
2020-05-31 21:12:57 -0700

    global block, aggregator tests

--- pyrographnets/blocks/__init__.py
+++ pyrographnets/blocks/__init__.py
@@ -1,6 +1,6 @@
 from pyrographnets.blocks.aggregator import Aggregator
 from pyrographnets.blocks.flex import Flex
 from pyrographnets.blocks.mlp import MLP
-from pyrographnets.blocks.edge import EdgeBlock, AggregatingEdgeBlock
-from pyrographnets.blocks.node import NodeBlock, AggregatingNodeBlock
+from pyrographnets.blocks.edge_block import EdgeBlock, AggregatingEdgeBlock
+from pyrographnets.blocks.node_block import NodeBlock, AggregatingNodeBlock
 from pyrographnets.blocks.global_block import GlobalBlock, AggregatingGlobalBlock
---
+++ tests/test_blocks/test_aggregator.py
@@ -0,0 +1,15 @@
+from pyrographnets.blocks import Aggregator
+import torch
+import pytest
+
+@pytest.mark.parametrize('method', ['mean', 'max', 'min', 'add'])
+def test_aggregators(method):
+    block = Aggregator(method)
+    idx = torch.tensor([0, 0, 0, 1, 1, 1, 1])
+    x = torch.tensor([0, 1, 10, 3, 4, 55, 6])
+    out = block(x, idx, dim=0)
+    print(out)
+
+def test_invalid_method():
+    with pytest.raises(ValueError):
+        block = Aggregator('not a method')
