4da29d8ae8f5b7618eb9599bac96a76dd8da0e88
Justin Vrana <justin.vrana@gmail.com>
2020-08-08 17:54:12 -0700

    updates

--- pyrographnets/blocks/global_block.py
+++ pyrographnets/blocks/global_block.py
@@ -5,36 +5,40 @@ from pyrographnets.data import GraphBatch
 
 
 class GlobalBlock(Block):
-
     def __init__(self, mlp):
-        super().__init__({'mlp': mlp}, independent=True)
+        super().__init__({"mlp": mlp}, independent=True)
 
     def forward(self, global_attr):
-        return self.block_dict['mlp'](global_attr)
+        return self.block_dict["mlp"](global_attr)
 
     def forward_from_data(self, data: GraphBatch):
         return self(data.g)
 
 
 class AggregatingGlobalBlock(GlobalBlock):
-
     def __init__(self, mlp, edge_aggregator=None, node_aggregator=None):
         super().__init__(mlp)
-        self.block_dict['edge_aggregator'] = edge_aggregator
-        self.block_dict['node_aggregator'] = node_aggregator
+        self.block_dict["edge_aggregator"] = edge_aggregator
+        self.block_dict["node_aggregator"] = node_aggregator
         self._independent = False
 
     def forward(self, global_attr, node_attr, edge_attr, edges, node_idx, edge_idx):
         aggregated = [global_attr]
-        if 'node_aggregator' in self.block_dict:
+        if "node_aggregator" in self.block_dict:
             aggregated.append(
-                self.block_dict['node_aggregator'](node_attr, node_idx, dim=0, dim_size=global_attr.shape[0]))
-        if 'edge_aggregator' in self.block_dict:
+                self.block_dict["node_aggregator"](
+                    node_attr, node_idx, dim=0, dim_size=global_attr.shape[0]
+                )
+            )
+        if "edge_aggregator" in self.block_dict:
             aggregated.append(
-                self.block_dict['edge_aggregator'](edge_attr, edge_idx, dim=0, dim_size=global_attr.shape[0]))
+                self.block_dict["edge_aggregator"](
+                    edge_attr, edge_idx, dim=0, dim_size=global_attr.shape[0]
+                )
+            )
 
         out = torch.cat(aggregated, dim=1)
-        return self.block_dict['mlp'](out)
+        return self.block_dict["mlp"](out)
 
     def forward_from_data(self, data: GraphBatch):
         return self(data.g, data.x, data.e, data.edges, data.node_idx, data.edge_idx)
--- pyrographnets/blocks/node_block.py
+++ pyrographnets/blocks/node_block.py
@@ -7,36 +7,45 @@ from pyrographnets.data import GraphBatch
 
 
 class NodeBlock(Block):
-
     def __init__(self, mlp: nn.Module):
-        super().__init__({
-            'mlp': mlp
-        }, independent=True)
+        super().__init__({"mlp": mlp}, independent=True)
 
     def forward(self, node_attr):
-        return self.block_dict['mlp'](node_attr)
+        return self.block_dict["mlp"](node_attr)
 
     def forward_from_data(self, data: GraphBatch):
         return self(data.x)
 
 
 class AggregatingNodeBlock(NodeBlock):
-
     def __init__(self, mlp: nn.Module, edge_aggregator: Aggregator):
         super().__init__(mlp)
-        self.block_dict['edge_aggregator'] = edge_aggregator
+        self.block_dict["edge_aggregator"] = edge_aggregator
         self._independent = False
 
     # TODO: source_to_target, target_to_source
-    def forward(self, node_attr, edge_attr, edges, global_attr: torch.Tensor = None, node_index: torch.Tensor = None):
+    def forward(
+        self,
+        node_attr,
+        edge_attr,
+        edges,
+        global_attr: torch.Tensor = None,
+        node_index: torch.Tensor = None,
+    ):
         # source_to_dest or dest_to_source
-        aggregated = (self.block_dict['edge_aggregator'](edge_attr, edges[1], dim=0, dim_size=node_attr.size(0)), )
+        aggregated = (
+            self.block_dict["edge_aggregator"](
+                edge_attr, edges[1], dim=0, dim_size=node_attr.size(0)
+            ),
+        )
         if global_attr is not None:
             if node_index is None:
-                raise RuntimeError("Must provide `node_index` if providing `global_attr`")
-            aggregated += (global_attr[node_index], )
+                raise RuntimeError(
+                    "Must provide `node_index` if providing `global_attr`"
+                )
+            aggregated += (global_attr[node_index],)
         out = torch.cat([node_attr, *aggregated], dim=1)
-        return self.block_dict['mlp'](out)
+        return self.block_dict["mlp"](out)
 
     def forward_from_data(self, data: GraphBatch):
         return self(data.x, data.e, data.edges)
