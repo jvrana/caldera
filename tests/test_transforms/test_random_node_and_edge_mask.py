2f08e32416c1c6781b86e8c715d87661c02a58b1
Justin Vrana <justin.vrana@gmail.com>
2020-08-21 09:15:06 -0700

    test reformatting

--- tests/test_transforms/test_random_node_and_edge_mask.py
+++ tests/test_transforms/test_random_node_and_edge_mask.py
@@ -3,7 +3,7 @@ from caldera.transforms import RandomNodeMask, RandomEdgeMask
 from caldera.data import GraphData, GraphBatch
 
 
-@pytest.mark.parametrize(
+parametrize_dropout = pytest.mark.parametrize(
     "dropout",
     [
         pytest.param(
@@ -24,12 +24,17 @@ from caldera.data import GraphData, GraphBatch
     ],
     ids=lambda x: "dropout=" + str(x),
 )
-@pytest.mark.parametrize(
+
+parametrize_random_data = pytest.mark.parametrize(
     "random_data",
     [(GraphData, None), (GraphBatch, None)],
     indirect=True,
     ids=lambda x: x[0].__name__,
 )
+
+
+@parametrize_dropout
+@parametrize_random_data
 def test_random_node_mask(random_data, dropout, seeds):
     transform = RandomNodeMask(dropout)
     data2 = transform(random_data)
@@ -44,33 +49,8 @@ def test_random_node_mask(random_data, dropout, seeds):
         assert data2.num_edges <= random_data.num_edges
 
 
-@pytest.mark.parametrize(
-    "dropout",
-    [
-        pytest.param(
-            -0.1,
-            marks=pytest.mark.xfail(
-                raises=ValueError, strict=True, reason="Dropout must be between 0 and 1"
-            ),
-        ),
-        pytest.param(
-            1.1,
-            marks=pytest.mark.xfail(
-                raises=ValueError, strict=True, reason="Dropout must be between 0 and 1"
-            ),
-        ),
-        0.5,
-        0.0,
-        1.0,
-    ],
-    ids=lambda x: "dropout=" + str(x),
-)
-@pytest.mark.parametrize(
-    "random_data",
-    [(GraphData, None), (GraphBatch, None)],
-    indirect=True,
-    ids=lambda x: x[0].__name__,
-)
+@parametrize_dropout
+@parametrize_random_data
 def test_random_edge_mask(random_data, dropout, seeds):
     transform = RandomEdgeMask(dropout)
     data2 = transform(random_data)
