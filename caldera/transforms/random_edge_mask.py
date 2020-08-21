5b1de3d317931920a4e296c5efb4f08c19da9434
Justin Vrana <justin.vrana@gmail.com>
2020-08-21 09:11:35 -0700

    added random transforms
    RandomEdgeMask, RandomNodeMask, Shuffle, RandomHop

--- caldera/transforms/random_edge_mask.py
+++ caldera/transforms/random_edge_mask.py
@@ -6,12 +6,20 @@ Apply random edge mask to the graph.
 from .base import TransformBase
 from caldera.data import GraphData, GraphBatch
 from typing import overload
+from torch import distributions
+import torch
 
 
 class RandomEdgeMask(TransformBase):
+    def __init__(self, dropout: float):
+        if dropout < 0.0 or dropout > 1.0:
+            raise ValueError("Edge dropout rate must be between [0., 1.]")
+        self.dist = distributions.Bernoulli(1.0 - dropout)
+
     @overload
     def __call__(self, data: GraphData) -> GraphData:
         ...
 
     def __call__(self, data: GraphBatch) -> GraphBatch:
-        pass
+        mask = self.dist.sample((data.num_edges,)).to(torch.bool)
+        return data.apply_edge_mask(mask)
--- tests/test_transforms/test_reverse.py
+++ tests/test_transforms/test_reverse.py
@@ -3,15 +3,13 @@ from caldera.transforms import Reverse
 from caldera.data import GraphBatch, GraphData
 
 
-def test_reverse_graph_data():
-    data = GraphData.random(5, 4, 3)
+def test_reverse_graph_data(random_data):
     reverse = Reverse()
-    reversed_data = reverse(data)
-    assert torch.all(reversed_data.edges.flip(1) == data.edges)
+    reversed_data = reverse(random_data)
+    assert torch.all(reversed_data.edges.flip(1) == random_data.edges)
 
 
-def test_reverse_graph_batch():
-    data = GraphBatch.random_batch(100, 5, 4, 3)
+def test_reverse_graph_batch(random_data):
     reverse = Reverse()
-    reversed_data = reverse(data)
-    assert torch.all(reversed_data.edges.flip(1) == data.edges)
+    reversed_data = reverse(random_data)
+    assert torch.all(reversed_data.edges.flip(1) == random_data.edges)
