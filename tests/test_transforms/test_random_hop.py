a38b04bd08560cdc92a3684e271597fa3ced13d1
Justin Vrana <justin.vrana@gmail.com>
2020-08-21 12:24:40 -0700

    Update test_random_hop.py

--- tests/test_transforms/test_random_hop.py
+++ tests/test_transforms/test_random_hop.py
@@ -22,3 +22,17 @@ def test_random_hop(random_data, seeds):
     hopped = hop(random_data)
     print(hopped.shape)
     print(hopped.size)
+
+
+@pytest.mark.parametrize(
+    "random_data",
+    [
+        (GraphBatch, None, (1000, 100, 50, 25), {"min_edges": 1, "min_nodes": 1}),
+    ],
+    ids=lambda x: x[0].__name__,
+    indirect=True,
+)
+def test_benchmark_hop(random_data):
+    hop = RandomHop(1, 2)
+    for i in range(100):
+        hop(random_data)
