b2d34bbaa74514cc6d5b76fe3916574a2c71daf5
Justin Vrana <justin.vrana@gmail.com>
2020-08-25 21:56:53 -0700

    update

--- tests/test_data/test_data_utils/test_torch_floyd_warshall.py
+++ tests/test_data/test_data_utils/test_torch_floyd_warshall.py
@@ -10,6 +10,24 @@ def test_floyd_warshall():
     print(W)
 
 
+import numpy as np
+
+
+# TODO: fast find neighbors
 def test_find_neighbors():
     data = GraphData.random(5, 4, 3, min_nodes=1000, min_edges=1000)
     W = floyd_warshall(data)
+    print(W.__class__)
+    nodes = torch.LongTensor([[0, 1, 2, 3]])
+
+    x = W[nodes]
+    noninf = x != float("inf")
+    reachable = x <= 3
+
+    print(x.__class__)
+    print(noninf.__class__)
+    c = np.logical_and(noninf, reachable)
+    d = np.where(c)
+    print(d)
+    neighbors = d[1]
+    print(neighbors)
