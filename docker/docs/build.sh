b14e5ef1461c0512900e80f1d6771633f9a42d2c
Justin Vrana <justin.vrana@gmail.com>
2020-09-07 07:42:54 -0700

    update build workflow

--- docker/docs/build.sh
+++ docker/docs/build.sh
@@ -19,4 +19,4 @@ echo "IMAGE ID: $IMAGE"
 docker run --cidfile $tmpfile $IMAGE /bin/bash -c "cd docs && make clean && make html"
 CID=$(cat $tmpfile)
 echo "CONTAINER ID: $CID"
-docker cp $CID:/src/docs/build $DEST
+docker cp $CID:/src/docs/build/html $DEST
